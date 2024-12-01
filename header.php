a719dafb142a72337b19285c19b3807622a600c4
OwPor <rayvincentconcepcion@gmail.com>
2024-12-01 21:37:55 +0800

    fixed purchase header

--- header.php
+++ header.php
@@ -34,24 +34,35 @@
                             </a>
                             <div class="dropdown-content" id="dropdownMenu">
                                 <?php
+                                    $current_url = $_SERVER['REQUEST_URI'];
+                                    $is_park_or_stall = strpos($current_url, 'park.php') !== false || strpos($current_url, 'stall.php') !== false;
+
                                     switch ($user['role']) {
                                         case 'Customer':
                                             echo '<a href="account.php"><i class="fa-regular fa-user"></i> Account</a>';
-                                            echo '<a href="purchase.php"><i class="fa-regular fa-credit-card"></i> Purchase</a>';
+                                            if ($is_park_or_stall) {
+                                                echo '<a href="purchase.php"><i class="fa-regular fa-credit-card"></i> Purchase</a>';
+                                            }
                                             echo '<a href="favorites.php"><i class="fa-regular fa-heart"></i> Favorites</a>';
                                             break;
+                                    
                                         case 'Stall Owner':
                                             echo '<a href="account.php"><i class="fa-regular fa-user"></i> Account</a>';
-                                            echo '<a href="purchase.php"><i class="fa-regular fa-credit-card"></i> Purchase</a>';
+                                            if ($is_park_or_stall) {
+                                                echo '<a href="purchase.php"><i class="fa-regular fa-credit-card"></i> Purchase</a>';
+                                            }
                                             echo '<a href="favorites.php"><i class="fa-regular fa-heart"></i> Favorites</a>';
                                             echo '<a href="orders.php"><i class="fa-regular fa-rectangle-list"></i> Orders</a>';
                                             echo '<a href="managemenu.php"><i class="fa-regular fa-window-restore"></i> Manage Menu</a>';
                                             echo '<a href="sales.php"><i class="fa-solid fa-chart-column"></i> Sales</a>';
                                             echo '<a href="stallpage.php"><i class="fa-regular fa-address-card"></i> Stall Page</a>';
                                             break;
+                                    
                                         case 'Park Owner':
                                             echo '<a href="account.php"><i class="fa-regular fa-user"></i> Account</a>';
-                                            echo '<a href="purchase.php"><i class="fa-regular fa-credit-card"></i> Purchase</a>';
+                                            if ($is_park_or_stall) {
+                                                echo '<a href="purchase.php"><i class="fa-regular fa-credit-card"></i> Purchase</a>';
+                                            }
                                             echo '<a href="favorites.php"><i class="fa-regular fa-heart"></i> Favorites</a>';
                                             echo '<a href="managestall.php"><i class="fa-regular fa-building"></i> Manage Stall</a>';
                                             echo '<a href="dashboard.php"><i class="fa-solid fa-table-columns"></i> Dashboard</a>';
