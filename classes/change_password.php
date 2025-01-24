43f5410650fb299fbe8e7432cd980a6933a8d9bc
Ray Vincent DS. Concepcion <rayvincentconcepcion@gmail.com>
2025-01-24 20:54:43 +0800

    Update change_password.php

--- classes/change_password.php
+++ classes/change_password.php
@@ -3,15 +3,12 @@
 require_once __DIR__ . '/db.class.php';
 session_start();
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form data
     $currentPassword = $_POST['currentpassword'];
     $newPassword = $_POST['newpassword'];
     $retypePassword = $_POST['retypepassword'];
     $logoutOtherDevices = isset($_POST['logout_other_devices']) ? 1 : 0;
 
-    // Validate the passwords
     if ($newPassword !== $retypePassword) {
         $_SESSION['error'] = "New passwords do not match.";
         header("Location: ../account.php");
