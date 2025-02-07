042db1ae4260c965dacef95f2c39c616c2313422
OwPor <rayvincentconcepcion@gmail.com>
2025-02-07 22:20:05 +0800

    added completed, cancelled, and scheduled

--- purchase.php
+++ purchase.php
@@ -25,7 +25,7 @@
         <a href="#preparing" class="nav-link" data-target="preparing">Preparing</a>
         <a href="#toreceive" class="nav-link" data-target="toreceive">To Receive</a>
         <a href="#completed" class="nav-link" data-target="completed">Completed</a>
-        <a href="#canceled" class="nav-link" data-target="canceled">Canceled</a>
+        <a href="#canceled" class="nav-link" data-target="canceled">Cancelled</a>
         <a href="#scheduled" class="nav-link" data-target="scheduled">Scheduled</a>
     </div>
     <form action="#" method="get" class="searchmenu rounded-2 mt-4 bg-white py-2 px-3">
@@ -390,48 +390,35 @@
         <div class="border py-3 px-4 rounded-2 bg-white mb-3">
             <div class="d-flex justify-content-between align-items-center border-bottom pb-2">
                 <div class="d-flex gap-3 align-items-center">
-                    <span class="fw-bold">ORDER ID: 0000</span>
+                    <span class="fw-bold">ORDER ID: <?= $order['order_id']; ?></span>
                     <span class="dot text-muted"></span>
                     <div class="d-flex gap-2 align-items-center">
-                        <span class="fw-bold">Food Stall Name</span>
+                        <span class="fw-bold"><?= $order['food_stall_name'] ?></span>
                         <button class="viewstall border bg-white small px-2" onclick="window.location.href='stall.php';">View Stall</button>
                     </div>
                 </div>
                 <div class="d-flex gap-3 align-items-center">
-                    <span style="color: #6A9C89" class="small">07/29/2024 22:59</span>
+                    <span style="color: #6A9C89" class="small"><?= $order['order_date'] ?></span>
                     <span class="dot text-muted"></span>
-                    <span class="fw-bold" style="color: #CD5C08">COMPLETED</span>
+                    <span class="fw-bold" style="color: #CD5C08"><?= $order['status'] ?></span>
                 </div>
             </div>
             <div class="d-flex justify-content-between border-bottom py-2">
                 <div class="d-flex gap-3 align-items-center">
                     <img src="assets/images/food1.jpg" width="85px" height="85px" class="border rounded-2">
                     <div>
-                        <span class="fs-5">Food Name</span><br>
-                        <span class="small text-muted">Variation: Chocolate, Medium</span><br>
+                        <span class="fs-5"><?= $order['food_name'] ?></span><br>
+                        <span class="small text-muted"><?= $order['formatted_variations']; ?></span><br>
                         <span>x1</span>
                     </div>
                 </div>
                 <div class="d-flex flex-column justify-content-end">
-                    <span class="fw-bold">₱103</span>
-                </div>
-            </div>
-            <div class="d-flex justify-content-between border-bottom py-2">
-                <div class="d-flex gap-3 align-items-center">
-                    <img src="assets/images/food2.jpg" width="85px" height="85px" class="border rounded-2">
-                    <div>
-                        <span class="fs-5">Food Name</span><br>
-                        <span class="small text-muted">Variation: Chocolate, Medium</span><br>
-                        <span>x1</span>
-                    </div>
-                </div>
-                <div class="d-flex flex-column justify-content-end">
-                    <span class="fw-bold">₱103</span>
+                    <span class="fw-bold">₱<?= $order['price'] ?></span>
                 </div>
             </div>
             <div class="d-flex justify-content-between pt-2">
                 <div class="d-flex gap-3 align-items-center text-muted small">
-                    <span>Payment Method: Cash</span>
+                    <span>Payment Method: <?= $order['payment_method'] ?></span>
                     <span class="dot text-muted"></span>
                     <span> Your order is completed</span>
                 </div>
@@ -447,57 +434,43 @@
                     </div>
                 </div>
             </div>
-            
         </div>
     </div>
     <div id="canceled" class="section-content d-none">
         <div class="border py-3 px-4 rounded-2 bg-white mb-3">
             <div class="d-flex justify-content-between align-items-center border-bottom pb-2">
                 <div class="d-flex gap-3 align-items-center">
-                    <span class="fw-bold">ORDER ID: 0000</span>
+                    <span class="fw-bold">ORDER ID: <?= $order['order_id'] ?></span>
                     <span class="dot text-muted"></span>
                     <div class="d-flex gap-2 align-items-center">
-                        <span class="fw-bold">Food Stall Name</span>
+                        <span class="fw-bold"><?= $order['food_stall_name'] ?></span>
                         <button class="viewstall border bg-white small px-2" onclick="window.location.href='stall.php';">View Stall</button>
                     </div>
                 </div>
                 <div class="d-flex gap-3 align-items-center">
-                    <span style="color: #6A9C89" class="small">07/29/2024 22:59</span>
+                    <span style="color: #6A9C89" class="small"><?= $order['order_date'] ?></span>
                     <span class="dot text-muted"></span>
-                    <span class="fw-bold" style="color: #CD5C08">CANCELED</span>
+                    <span class="fw-bold" style="color: #CD5C08"><?= $order['status'] ?></span>
                 </div>
             </div>
             <div class="d-flex justify-content-between border-bottom py-2">
                 <div class="d-flex gap-3 align-items-center">
                     <img src="assets/images/food1.jpg" width="85px" height="85px" class="border rounded-2">
                     <div>
-                        <span class="fs-5">Food Name</span><br>
-                        <span class="small text-muted">Variation: Chocolate, Medium</span><br>
+                        <span class="fs-5"><?= $order['food_name'] ?></span><br>
+                        <span class="small text-muted"><?= $order['formatted_variations']; ?></span><br>
                         <span>x1</span>
                     </div>
                 </div>
                 <div class="d-flex flex-column justify-content-end">
-                    <span class="fw-bold">₱103</span>
-                </div>
-            </div>
-            <div class="d-flex justify-content-between border-bottom py-2">
-                <div class="d-flex gap-3 align-items-center">
-                    <img src="assets/images/food2.jpg" width="85px" height="85px" class="border rounded-2">
-                    <div>
-                        <span class="fs-5">Food Name</span><br>
-                        <span class="small text-muted">Variation: Chocolate, Medium</span><br>
-                        <span>x1</span>
-                    </div>
-                </div>
-                <div class="d-flex flex-column justify-content-end">
-                    <span class="fw-bold">₱103</span>
+                    <span class="fw-bold">₱<?= $order['price'] ?></span>
                 </div>
             </div>
             <div class="d-flex justify-content-between pt-2">
                 <div class="d-flex gap-3 align-items-center text-muted small">
-                    <span>Payment Method: Cash</span>
+                    <span>Payment Method: <?= $order['payment_method'] ?></span>
                     <span class="dot text-muted"></span>
-                    <span>Canceled by you (Don't want to buy anymore)</span>
+                    <span>Cancelled by you (Don't want to buy anymore)</span>
                 </div>
                 <div class="d-flex gap-4 align-items-center">
                     <div class="d-flex gap-2">
@@ -518,48 +491,35 @@
         <div class="border py-3 px-4 rounded-2 bg-white mb-3">
             <div class="d-flex justify-content-between align-items-center border-bottom pb-2">
                 <div class="d-flex gap-3 align-items-center">
-                    <span class="fw-bold">ORDER ID: 0000</span>
+                    <span class="fw-bold">ORDER ID: <?= $order['order_id'] ?></span>
                     <span class="dot text-muted"></span>
                     <div class="d-flex gap-2 align-items-center">
-                        <span class="fw-bold">Food Stall Name</span>
+                        <span class="fw-bold"><?= $order['food_stall_name'] ?></span>
                         <button class="viewstall border bg-white small px-2" onclick="window.location.href='stall.php';">View Stall</button>
                     </div>
                 </div>
                 <div class="d-flex gap-3 align-items-center">
-                    <span style="color: #6A9C89" class="small">07/29/2024 22:59</span>
+                    <span style="color: #6A9C89" class="small"><?= $order['order_date'] ?></span>
                     <span class="dot text-muted"></span>
-                    <span class="fw-bold" style="color: #CD5C08">SCHEDULED</span>
+                    <span class="fw-bold" style="color: #CD5C08"><?= $order['status'] ?></span>
                 </div>
             </div>
             <div class="d-flex justify-content-between border-bottom py-2">
                 <div class="d-flex gap-3 align-items-center">
                     <img src="assets/images/food1.jpg" width="85px" height="85px" class="border rounded-2">
                     <div>
-                        <span class="fs-5">Food Name</span><br>
-                        <span class="small text-muted">Variation: Chocolate, Medium</span><br>
+                        <span class="fs-5"><?= $order['food_name'] ?></span><br>
+                        <span class="small text-muted"><?= $order['formatted_variations'] ?></span><br>
                         <span>x1</span>
                     </div>
                 </div>
                 <div class="d-flex flex-column justify-content-end">
-                    <span class="fw-bold">₱103</span>
-                </div>
-            </div>
-            <div class="d-flex justify-content-between border-bottom py-2">
-                <div class="d-flex gap-3 align-items-center">
-                    <img src="assets/images/food2.jpg" width="85px" height="85px" class="border rounded-2">
-                    <div>
-                        <span class="fs-5">Food Name</span><br>
-                        <span class="small text-muted">Variation: Chocolate, Medium</span><br>
-                        <span>x1</span>
-                    </div>
-                </div>
-                <div class="d-flex flex-column justify-content-end">
-                    <span class="fw-bold">₱103</span>
+                    <span class="fw-bold">₱<?= $order['price'] ?></span>
                 </div>
             </div>
             <div class="d-flex justify-content-between pt-2">
                 <div class="d-flex gap-3 align-items-center text-muted small">
-                    <span>Payment Method: Cash</span>
+                    <span>Payment Method: <?= $order['payment_method'] ?></span>
                     <span class="dot text-muted"></span>
                     <span>Scheduled on October 15, 2024 at 1:00 PM</span>
                 </div>
